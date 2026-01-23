15befb170f04b27350554d84d468178064b5cb8e
Parth R. Bhanderi <parthbhanderi@Parths-MacBook-Air.local>
2026-01-23 10:58:42 +0530

    fix: Update build script paths to match project structure

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 set -o errexit
 
 pip install -r requirements.txt
-python backend/manage.py collectstatic --no-input
-python backend/manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
